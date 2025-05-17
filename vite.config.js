4580469f02225403e3e20e0c300e7f4f23385942
theigniteai <theignite.ai@gmail.com>
2025-05-17 20:15:37 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// AccentShift Frontend (vite.config.js)
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
   }
-})
+});
