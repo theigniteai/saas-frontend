15244153d4110ad0ba7789c0d3f58a91ff17d96e
theigniteai <theignite.ai@gmail.com>
2025-05-17 22:00:08 +0400

    Create api.js

---
+++ src/api.js
@@ -0,0 +1,11 @@
+// src/api.js
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default api;
